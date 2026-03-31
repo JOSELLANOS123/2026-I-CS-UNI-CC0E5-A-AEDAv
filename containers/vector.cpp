580295ec6868f1acd64fd9beecdd5a7ebcbd4d7e
ecuadros <ecuadros@spc.org.pe>
2026-03-31 07:58:03 -0500

    refactor: convert Vector class to a template to support generic types

--- containers/vector.cpp
+++ containers/vector.cpp
@@ -4,33 +4,8 @@
 
 using namespace std;
 
-Vector::Vector(size_t capacity){
-    m_capacity = capacity;
-    m_size = 0;
-    m_data = new T1[capacity];
-}
-
-Vector::~Vector(){
-    delete[] m_data;
-}
-
-void Vector::push_back(T1 value){
-    if(m_size < m_capacity)
-        m_data[m_size++] = value;
-}
-
-T1 Vector::get(size_t index){
-    if(index >= 0 && index < m_size)
-        return m_data[index];
-    return -1;
-}
-
-size_t Vector::size(){
-    return m_size;
-}
-
 void DemoVector(){
-    Vector v(10);
+    Vector<T1> v(10);
     v.push_back(1);
     v.push_back(2);
     v.push_back(-1);
@@ -39,4 +14,6 @@ void DemoVector(){
         std::cout << v.get(i) << " ";
     }
     std::cout << std::endl;
+
+
 }
--- containers/vector.h
+++ containers/vector.h
@@ -4,18 +4,50 @@
 #include <cstddef> // size_t
 #include "../types.h"
 
+template <typename T>
 class Vector{
+    using value_type = T;
 private:
     size_t  m_capacity;
     size_t  m_size;
-    T1 * m_data;
+    T * m_data;
 public:
     Vector(size_t capacity);
     virtual ~Vector();
-    virtual void push_back(T1 value);
-    virtual T1  get(size_t index);
+    virtual void push_back(T value);
+    virtual T  get(size_t index);
     virtual size_t  size();
 };
+
+template <typename T>
+Vector<T>::Vector(size_t capacity){
+    m_capacity = capacity;
+    m_size = 0;
+    m_data = new T[capacity];
+}
+
+template <typename T>
+Vector<T>::~Vector(){
+    delete[] m_data;
+}
+
+template <typename T>
+void Vector<T>::push_back(T value){
+    if(m_size < m_capacity)
+        m_data[m_size++] = value;
+}
+
+template <typename T>
+T Vector<T>::get(size_t index){
+    if(index >= 0 && index < m_size)
+        return m_data[index];
+    return -1;
+}
+
+template <typename T>
+size_t Vector<T>::size(){
+    return m_size;
+}
 
 void DemoVector();
 
